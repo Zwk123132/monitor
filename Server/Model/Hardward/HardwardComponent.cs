9f3daab0aa4392eb7421d45cc0ab043ea65a85e4
Administrator <Administrator@DESKTOP-78GBUML>
2025-03-15 18:32:09 +0800

    添加项目文件。

---
+++ Server/Model/Hardward/HardwardComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibreHardwareMonitor.Hardware;
+using Computer = LibreHardwareMonitor.Hardware.Computer;
+
+
+
+namespace ET
+{
+    [ComponentOf(typeof(Scene))]
+    public class HardwardComponent:Entity,IAwake,IDestroy
+    {
+        public Computer m_Computer;
+    }
+}
---
+++ Server/Model/Hardward/HardwardComponentSystem.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibreHardwareMonitor.Hardware;
+
+
+namespace ET
+{
+    [FriendClass(typeof(HardwardComponent))]
+    public class HardwardComponent_Awake : AwakeSystem<HardwardComponent>
+    {
+        public override void Awake(HardwardComponent self)
+        {
+            Console.WriteLine("硬件初始化完成");
+            self.m_Computer = self.NormalSetting();
+            self.m_Computer.Open();
+
+        }
+    }
+    [FriendClass(typeof(HardwardComponent))]
+    public class HardwardComponent_Destroy : DestroySystem<HardwardComponent>
+    {
+        public override void Destroy(HardwardComponent self)
+        {
+            self.m_Computer?.Close();
+            self.m_Computer = null;
+        }
+    }
+
+    [FriendClass(typeof(HardwardComponent))]
+    public static class HardwardComponentSystem
+    {
+        public static Computer NormalSetting(this HardwardComponent self)
+        {
+            return new Computer()
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = true,
+                IsMemoryEnabled = true,
+                IsMotherboardEnabled = false,
+                IsControllerEnabled = false,
+                IsNetworkEnabled = false,
+                IsStorageEnabled = false,
+                IsBatteryEnabled =false
+            };
+        }
+        public static Computer AllSetting()
+        {
+            return new Computer()
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = true,
+                IsMemoryEnabled = true,
+                IsMotherboardEnabled = true,
+                IsControllerEnabled = true,
+                IsNetworkEnabled = true,
+                IsStorageEnabled = true,
+                IsBatteryEnabled = true
+            };
+        }
+
+        public static void cd(this HardwardComponent self)
+        {
+
+        
+        }
+    }
+}
